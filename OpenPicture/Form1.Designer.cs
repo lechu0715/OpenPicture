a55bb3fea22459b57e0c4f975124ca5e7e40ee1d
Lechosław Parol <lechu0715@gmail.com>
2021-03-22 10:08:38 +0100

    Add project files.

---
+++ OpenPicture/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿
+namespace OpenPicture
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbPhoto = new System.Windows.Forms.PictureBox();
+            this.btnAddPhoto = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPhoto)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbPhoto
+            // 
+            this.pbPhoto.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pbPhoto.Location = new System.Drawing.Point(12, 12);
+            this.pbPhoto.Name = "pbPhoto";
+            this.pbPhoto.Size = new System.Drawing.Size(398, 341);
+            this.pbPhoto.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbPhoto.TabIndex = 0;
+            this.pbPhoto.TabStop = false;
+            // 
+            // btnAddPhoto
+            // 
+            this.btnAddPhoto.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnAddPhoto.Location = new System.Drawing.Point(12, 359);
+            this.btnAddPhoto.Name = "btnAddPhoto";
+            this.btnAddPhoto.Size = new System.Drawing.Size(133, 79);
+            this.btnAddPhoto.TabIndex = 1;
+            this.btnAddPhoto.Text = "Dodaj Zdjęcie";
+            this.btnAddPhoto.UseVisualStyleBackColor = true;
+            this.btnAddPhoto.Click += new System.EventHandler(this.btnAddPhoto_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnDelete.Location = new System.Drawing.Point(152, 360);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(125, 78);
+            this.btnDelete.TabIndex = 2;
+            this.btnDelete.Text = "Kasuj Zdjęcie";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(422, 450);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAddPhoto);
+            this.Controls.Add(this.pbPhoto);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            ((System.ComponentModel.ISupportInitialize)(this.pbPhoto)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbPhoto;
+        private System.Windows.Forms.Button btnAddPhoto;
+        private System.Windows.Forms.Button btnDelete;
+    }
+}
+
---
+++ OpenPicture/Form1.cs
@@ -0,0 +1,83 @@
+﻿using OpenPicture.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OpenPicture
+{
+    public partial class Form1 : Form
+    {
+        
+        public string PathFileText { get; set; } = $@"{Path.GetDirectoryName(Application.ExecutablePath)}\Sciezka.txt";
+        public string NewPath { get; set; } = @"";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            if (File.Exists(PathFileText))
+            {
+                string text = File.ReadAllText(PathFileText);
+                pbPhoto.Image = new Bitmap(@text);
+            }
+
+            ButtonsVisible();
+
+        }
+
+        public void CreateTextFile(string _pathPhoto)
+        {
+            File.WriteAllText(PathFileText, _pathPhoto);
+        }
+
+
+        public void ButtonsVisible()
+        {
+            if (pbPhoto.Image == null)
+            {
+                btnDelete.Visible = false;
+            }
+            else
+            {
+                btnDelete.Visible = true;
+            }
+        }
+
+        public void btnAddPhoto_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+
+            open.Filter = "Image Files(*.jpg; *.jpeg; *.gif; *.bmp)|*.jpg; *.jpeg; *.gif; *.bmp";
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                pbPhoto.Image = new Bitmap(open.FileName);
+                pbPhoto.Image = new Bitmap(open.FileName);
+                NewPath = open.FileName.ToString();
+
+                CreateTextFile(NewPath);
+            }
+            ButtonsVisible();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            pbPhoto.Image = null;
+            ButtonsVisible();
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (pbPhoto.Image == null)
+            {
+                File.Delete(PathFileText);
+            }
+        }
+    }
+}
